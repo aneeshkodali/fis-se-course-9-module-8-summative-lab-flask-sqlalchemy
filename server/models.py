3088c2d7c5d852e7aecf51f11c50d5c64d7b4fa6
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-04-25 14:25:08 -0400

    Add model relationships

--- server/models.py
+++ server/models.py
@@ -9,16 +9,29 @@ class Exercise(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    category = db.Column(db.String)
-    equipment_needed = db.Column(db.Boolean)
+    category = db.Column(db.String, nullable=False)
+    equipment_needed = db.Column(db.Boolean, nullable=False)
+
+    # has many with WorkoutExercise
+    workout_exercises = db.relationship('WorkoutExercise', back_populates='exercise', cascade='all, delete-orphan')
+
+    # has many with Workout (through WorkoutExercise)
+    workouts = db.relationship('Workout', secondary='workout_exercises', back_populates='exercises', viewonly=True)
 
 class Workout(db.Model):
     __tablename__ = 'workouts'
 
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, server_default=db.func.now())
+    date = db.Column(db.Date)
     duration_minutes = db.Column(db.Integer, nullable=False)
     notes = db.Column(db.Text)
+
+    # has many WorkoutExercise
+    workout_exercises = db.relationship('WorkoutExercise', back_populates='workout', cascade='all, delete-orphan')
+
+    # has many with Exercise (through WorkoutExercise)
+    exercises = db.relationship('Exercise', secondary='workout_exercises', back_populates='workouts', viewonly=True)
+
 
 class WorkoutExercise(db.Model):
     __tablename__ = 'workout_exercises'
@@ -29,4 +42,10 @@ class WorkoutExercise(db.Model):
     reps = db.Column(db.Integer, nullable=False)
     sets = db.Column(db.Integer, nullable=False)
     duration_seconds = db.Column(db.Integer, nullable=False)
+
+    # belongs to Workout
+    workout = db.relationship('Workout', back_populates='workout_exercises')
+
+    # belongs to Exercise
+    exercise = db.relationship('Exercise', back_populates='workout_exercises')
 
