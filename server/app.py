6714cd9fc6fdadf3765c36b9f87e4cce7bb01fdc
Aneesh Kodali <aneesh.kodali@gmail.com>
2026-04-25 21:22:17 -0400

    Create workout endpoints

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 
 from models import *
@@ -12,6 +12,88 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 # Define Routes here
+@app.route('/workouts', methods=['GET'])
+def get_workouts():
+
+    # initialize workouts list
+    workouts = []
+
+    # loop through Workouts and return dict
+    for workout in Workout.query.all():
+        workouts.append({
+            'id': workout.id,
+            'date': workout.date,
+            'duration_minutes': workout.duration_minutes,
+            'notes': workout.notes,
+        })
+
+    # return list
+    return jsonify(workouts), 200
+
+@app.route('/workouts/<id>:int')
+def get_workout(id):
+
+    # query for workout
+    workout = Workout.query.filter(Workout.id == id).first()
+
+    # return error if not found
+    if not workout:
+        msg = {'error': f"Workout with ID {id} not found"}
+        return make_response(msg, 404)
+    
+    # return workout
+    workout_dict = {
+        'id': workout.id,
+        'date': workout.date,
+        'duration_minutes': workout.duration_minutes,
+        'notes': workout.notes,
+        'exercises': [
+            {
+                'id': exercise.id,
+                'name': exercise.name,
+            }
+            for exercise in workout.exercises
+        ]
+    }
+    return jsonify(workout_dict), 200
+
+@app.route('/workouts', methods=['POST'])
+def create_workout():
+
+    # get data from request
+    data = request.get_json()
+    
+    # create new workout object
+    workout = Workout(
+        date=data.get('date'),
+        duration_minutes=data.get('duration_minutes')
+        notes=data.get('notes')
+    )
+
+    # commit
+    db.session.add(workout)
+    db.session.commit()
+
+    # return
+    return jsonify({'id': workout.id}), 201
+
+@app.route('/workouts/<id>:int', methods=['DELETE'])
+def delete_workout(id):
+
+    # query for workout
+    workout = Workout.query.filter(Workout.id == id).first()
+
+    # return error if not found
+    if not workout:
+        msg = {'error': f"Workout with ID {id} not found"}
+        return make_response(msg, 404)
+    
+    # commit
+    db.session.delete(workout)
+    db.session.commit()
+
+    # return
+    return jsonify({'message': f"Workout with ID {workout.id} deleted"}), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
